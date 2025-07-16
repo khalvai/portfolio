f90ff1a79325719c5ad8a37a97f394fd73ead9fb
khalvai <78291644+khalvai@users.noreply.github.com>
2025-07-16 12:16:39 +0330

    fix: inserted about me and degree.

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,15 +1,9 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
-  },
-  {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'Vali-e-Asr University of Rafsanjan',
+    degree: 'B.S. Computer Engineering',
+    link: 'http://vru.ac.ir/en',
+    year: '2020 to 2024',
   },
 ];
 
