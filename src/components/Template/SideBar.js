e4a808017ccf7ee85efd1b424e3e5a7f7438146a
Khalvai <78291644+khalvai@users.noreply.github.com>
2026-06-11 00:24:47 +0200

    fix: changed the photo of me.

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -9,7 +9,7 @@ const SideBar = () => (
   <section id="sidebar">
     <section id="intro">
       <Link to="/" className="logo">
-        <img src={`${PUBLIC_URL}/images/2.png`} alt="" />
+        <img src={`${PUBLIC_URL}/images/2.webp`} alt="" />
       </Link>
       <header>
         <h2>Khalvai Ozbek</h2>
